dd0fd20fa86f5485dcd93e7b9f9d88403712ff37
Lucas Gomes Silva <lucasgomessilva713@gmail.com>
2025-12-03 17:50:46 -0300

    criando a funcionalidade do grid

---
+++ src/components/Grid/styles.js
@@ -0,0 +1,22 @@
+import style, { styled } from "styled-components"
+
+export const Table = styled.table`
+    width: 98%;
+    background-color:#fff;
+    padding: 20px;
+    box-shadow: 0px 0px 5px #ccc;
+    borde-ridius: 5px;
+    max-width: 1120px;
+    margin: 20px auto
+`;
+
+export const Thead = styled.thead``;
+export const Tbody = styled.tbody``;
+export const Tr = styled.tr``;
+export const Th = styled.th`
+    text-align: start;
+    border-bottom inset;
+    padding-bottom: 5px
+    tex-aling: ${(props) => (props.alignCenter ? "center" : "start")};
+    width: ${(props) => (props.width ? props.width + "%" : "auto")};
+`;
